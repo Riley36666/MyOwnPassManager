67b7f455573df2417a10b52e87eb6b2c547ab6a4
Riley36666 <Riley.36666@gmail.com>
2026-02-15 20:12:53 +0000

    update to Systeminfos and allows cmds to be ran

--- src/System_info.py
+++ src/System_info.py
@@ -1,5 +1,35 @@
+import platform
+import socket
 import uuid
+import subprocess
 
+
+def run_cmd(cmd):
+    try:
+        return subprocess.check_output(cmd, shell=True, text=True).strip()
+    except Exception:
+        return None
+
+def get_pc_info():
+    info = {}
+
+
+    info["OS"] = platform.system()
+    info["OS Release"] = platform.release()
+    info["OS Version"] = platform.version()
+    info["Architecture"] = platform.machine()
+    info["Processor"] = platform.processor()
+    info["Python Version"] = platform.python_version()
+
+    # Host info
+    info["Hostname"] = socket.gethostname()
+    info["IP Address"] = socket.gethostbyname(socket.gethostname())
+    info["MAC Address"] = ':'.join(
+        f"{(uuid.getnode() >> i) & 0xff:02x}"
+        for i in range(0, 48, 8)
+    )
+
+    return info
 
 def getMACaddress():
     mac = uuid.getnode()
